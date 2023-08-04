544c52d4ed3b0e65041ec28107e393c042052dfe
hesam <bhesam2000@yahoo.com>
2023-08-04 19:41:53 +0330

    finally something working

--- vitrin/vitrin_react/vite.config.js
+++ vitrin/vitrin_react/vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    host: true,
+    port:5173,
+  },
   plugins: [react()],
+  build:{
+    outDir:'./build'
+  }
 })
