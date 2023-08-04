3f98ebdf8696b9c1359bcdd519d5411d74163014
hesam <bhesam2000@yahoo.com>
2023-08-04 20:53:25 +0330

    vitring container completed

--- vitrin/vitrin_react/vite.config.js
+++ vitrin/vitrin_react/vite.config.js
@@ -3,9 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    host: true,
-    port:5173,
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5173, // you can replace this port with any port
   },
   plugins: [react()],
   build:{
